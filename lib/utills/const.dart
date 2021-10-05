a5882bf7b6ce20094d49cced128973f644612ddc
Thanos <iamthegreatthanos@gmail.com>
2021-10-05 14:10:41 +0600

    Added baseUrl const

--- lib/utills/const.dart
+++ lib/utills/const.dart
@@ -10,8 +10,7 @@ class AppColors {
 
 
 class AppConstants {
-  static final String baseUrl = "https://test.kz/";
-  static final String phone = "phone";
+  static final String baseUrl = "https://boszhan.kz/";
+  static final String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvYm9zemhhbi5relwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTYzMzM0MzQyMiwibmJmIjoxNjMzMzQzNDIyLCJqdGkiOiJpUkNtd1Badk9hZzhSb2xpIiwic3ViIjo4MiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.72aix4ZOWamHRFSBDln3ogVTWqFqVVvZ9tC7Vus5SSI";
   static final String uid = "uid";
-  static final String name = "name";
 }
