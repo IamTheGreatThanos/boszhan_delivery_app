adf466e5e43c8976ec3380983ffd4fffe5d00891
Dinmukhammed <iamthegreatthanos@gmail.com>
2021-10-05 13:57:16 +0600

    Created const.dart

---
+++ lib/utills/const.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+
+class AppColors {
+  static final Color mainColor = Colors.red;
+  static final Color primaryColor = Colors.grey;
+}
+
+
+
+class AppConstants {
+  static final String baseUrl = "https://test.kz/";
+  static final String phone = "phone";
+  static final String uid = "uid";
+  static final String name = "name";
+}
