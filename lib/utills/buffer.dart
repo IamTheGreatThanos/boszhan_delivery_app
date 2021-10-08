185c833f5a71d4101711beb94ea6464b4da66919
Dinmukhammed <iamthegreatthanos@gmail.com>
2021-10-08 08:35:14 +0600

    Buffer

--- lib/utills/buffer.dart
+++ lib/utills/buffer.dart
@@ -1,1 +1,33 @@
 // Buffer
+
+// SizedBox(
+//   height: 60,
+//   child: InputDecorator(
+//     decoration: InputDecoration(
+//       labelStyle: TextStyle(color: Colors.black),
+//       errorStyle: TextStyle(color: Colors.redAccent, fontSize: 16),
+//       border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0))),
+//       isEmpty: false,
+//       child: DropdownButtonHideUnderline(
+//         child: DropdownButton(
+//         value: _value,
+//         items: [
+//           DropdownMenuItem(
+//             child: Text("First Item"),
+//             value: 1,
+//           ),
+//           DropdownMenuItem(
+//             child: Text("Second Item"),
+//             value: 2,
+//             )
+//         ],
+//         onChanged: (value){
+//           setState((){
+//             _value = value;
+//           });
+//         },
+//         hint:Text("Select item")
+//       )
+//     )
+//   ),
+// ),
