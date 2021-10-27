ef6d8a57816e8f902a471d1f056fc0dd15be5568
Thanos <iamthegreatthanos@gmail.com>
2021-10-27 10:00:02 +0600

    Added launching when printing.

--- lib/views/authorizationPage/authorization_page.dart
+++ lib/views/authorizationPage/authorization_page.dart
@@ -1,11 +1,11 @@
 import 'package:boszhan_delivery_app/services/auth_api_provider.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:http/http.dart' as http;
 import 'package:boszhan_delivery_app/views/home_page.dart';
 import 'package:boszhan_delivery_app/widgets/app_bar.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 import '../../main.dart';
 
 class LoginPage extends StatefulWidget {
@@ -23,8 +23,8 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   void initState() {
-    emailController.text = 'driver1@mail.ru';
-    passwordController.text = '123456';
+    // emailController.text = 'driver1@mail.ru';
+    // passwordController.text = '123456';
 
     getFCMToken();
 
@@ -33,19 +33,18 @@ class _LoginPageState extends State<LoginPage> {
       AndroidNotification? android = message.notification?.android;
       if (notification != null && android != null) {
         flutterLocalNotificationsPlugin.show(
-          notification.hashCode,
-          notification.title,
-          notification.body,
-          NotificationDetails(
-            android: AndroidNotificationDetails(
-              channel.id,
-              channel.name,
-              color: Colors.blue,
-              playSound: true,
-              icon: '@mipmap/ic_launcher',
-            ),
-          )
-        );
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                color: Colors.blue,
+                playSound: true,
+                icon: '@mipmap/ic_launcher',
+              ),
+            ));
       }
     });
 
@@ -54,19 +53,18 @@ class _LoginPageState extends State<LoginPage> {
       AndroidNotification? android = message.notification?.android;
       if (notification != null && android != null) {
         showDialog(
-          context: context,
-          builder: (_) {
-            return AlertDialog(
-              title: Text(notification.title.toString()),
-              content: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [Text(notification.body.toString())],
+            context: context,
+            builder: (_) {
+              return AlertDialog(
+                title: Text(notification.title.toString()),
+                content: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [Text(notification.body.toString())],
+                  ),
                 ),
-              ),
-            );
-          }
-        );
+              );
+            });
       }
     });
 
@@ -78,114 +76,109 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
         appBar: PreferredSize(
             preferredSize: const Size.fromHeight(60.0),
-            child: buildAppBar('Авторизация')
-        ),
+            child: buildAppBar('Авторизация')),
         body: SingleChildScrollView(
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.asset(
-                      "assets/images/logo.png",
-                      width: MediaQuery.of(context).size.width*0.5
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 10),
-                    child: TextFormField(
-                      controller: emailController,
-                      style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 20
+              Image.asset("assets/images/logo.png",
+                  width: MediaQuery.of(context).size.width * 0.5),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 80, vertical: 10),
+                child: TextFormField(
+                  controller: emailController,
+                  style: const TextStyle(color: Colors.black, fontSize: 20),
+                  decoration: const InputDecoration(
+                      fillColor: Colors.red,
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red)),
+                      hintText: 'Введите логин',
+                      helperText: 'Your login to enter the app.',
+                      labelText: 'Логин',
+                      labelStyle:
+                          TextStyle(color: Colors.black87, fontSize: 20),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red)),
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.black87,
                       ),
-                      decoration: const InputDecoration(
-                          fillColor: Colors.red,
-                          border: OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
-                          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
-                          hintText: 'Введите логин',
-                          helperText: 'Your login to enter the app.',
-                          labelText: 'Логин',
-                          labelStyle: TextStyle(color: Colors.black87, fontSize: 20),
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
-                          prefixIcon: Icon(
-                            Icons.person,
-                            color: Colors.black87,
-                          ),
-                          hintStyle: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 20
-                          ),
-                          prefixText: ' ',
-                          // suffixText: 'USD',
-                          suffixStyle: TextStyle(color: Colors.black, fontSize: 20)
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 20),
+                      prefixText: ' ',
+                      // suffixText: 'USD',
+                      suffixStyle:
+                          TextStyle(color: Colors.black, fontSize: 20)),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 80, vertical: 10),
+                child: TextFormField(
+                  controller: passwordController,
+                  style: const TextStyle(color: Colors.black, fontSize: 20),
+                  decoration: const InputDecoration(
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 20),
+                      fillColor: Colors.red,
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red)),
+                      hintText: 'Пароль',
+                      helperText: 'Your password to enter the app.',
+                      labelText: 'Пароль',
+                      labelStyle:
+                          TextStyle(color: Colors.black87, fontSize: 20),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red)),
+                      prefixIcon: Icon(
+                        Icons.vpn_key,
+                        color: Colors.black87,
                       ),
+                      prefixText: '',
+                      suffixStyle:
+                          TextStyle(color: Colors.black, fontSize: 20)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  height: MediaQuery.of(context).size.width * 0.07,
+                  child: ElevatedButton.icon(
+                    icon: const Icon(Icons.login, color: Colors.white),
+                    label: const Text('ВОЙТИ'),
+                    onPressed: () {
+                      login();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red,
+                      textStyle:
+                          const TextStyle(color: Colors.white, fontSize: 20),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 10),
-                    child: TextFormField(
-                      controller: passwordController,
-                      style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 20
-                      ),
-                      decoration: const InputDecoration(
-                          hintStyle: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 20
-                          ),
-                          fillColor: Colors.red,
-                          border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-                          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
-                          hintText: 'Пароль',
-                          helperText: 'Your password to enter the app.',
-                          labelText: 'Пароль',
-                          labelStyle: TextStyle(color: Colors.black87, fontSize: 20),
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
-                          prefixIcon: Icon(
-                            Icons.vpn_key,
-                            color: Colors.black87,
-                          ),
-                          prefixText: '',
-                          suffixStyle: TextStyle(color: Colors.black, fontSize: 20)
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width*0.3,
-                      height: MediaQuery.of(context).size.width*0.07,
-                      child: ElevatedButton.icon(
-                        icon: const Icon(Icons.login, color: Colors.white),
-                        label: const Text('ВОЙТИ'),
-                        onPressed: () {
-                          login();
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.red,
-                          textStyle: const TextStyle(color: Colors.white,fontSize: 20),
-                        ),
-                      ),
-                    ),
-                  ),
-                ])
-        )
-    );
+                ),
+              ),
+            ])));
   }
 
-  void login() async{
+  void login() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    var response = await AuthProvider().login(emailController.text, passwordController.text);
+    var response = await AuthProvider()
+        .login(emailController.text, passwordController.text);
     // TODO: Действие при авторизации пользователя...
     // print(response);
-    if (response != 'Error'){
+    if (response != 'Error') {
       prefs.setString("token", response['token']);
       prefs.setInt("user_id", response['user']['id']);
       prefs.setString("full_name", response['user']['full_name']);
       // print(response['token']);
 
-      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-    }
-    else{
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
         content: Text("Something went wrong.", style: TextStyle(fontSize: 20)),
       ));
