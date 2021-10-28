a50523f4d822dc3bb382b74e939061d2a2e1e05b
Thanos <iamthegreatthanos@gmail.com>
2021-10-28 16:42:50 +0600

    Bugs fixed in order.

--- lib/components/product_card.dart
+++ lib/components/product_card.dart
@@ -1,26 +1,49 @@
 import 'package:boszhan_delivery_app/models/basket.dart';
 import 'package:flutter/material.dart';
 
-class ProductCard extends StatelessWidget{
+class ProductCard extends StatelessWidget {
   const ProductCard(this.basket);
   final Basket basket;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: const CircleAvatar(
-        backgroundColor: Colors.amber,
-        child: Icon(Icons.shopping_cart_outlined, color: Colors.white,)
-      ),
-      title: Text(basket.name, style: TextStyle(fontSize: 20),),
-      subtitle: Row(
-        children: [
-          Text('Количество: ' + basket.count.toString() + (basket.measureId == 1 ? ' шт' : ' кг'), style: TextStyle(fontSize: 18)),
-          Spacer(),
-          Text('Сумма:  ' + basket.price.toString() + ' ₸', style: TextStyle(fontSize: 18))
-        ],
-      ),
-      onTap: (){}
-    );
+        leading: const CircleAvatar(
+            backgroundColor: Colors.amber,
+            child: Icon(
+              Icons.shopping_cart_outlined,
+              color: Colors.white,
+            )),
+        title: Text(
+          basket.name,
+          style: TextStyle(fontSize: 20),
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(
+              children: [
+                Text(
+                    'Количество: ' +
+                        basket.count.toString() +
+                        (basket.measureId == 1 ? ' шт' : ' кг'),
+                    style: TextStyle(fontSize: 18)),
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                      'Сумма(шт,кг):  ' + basket.price.toString() + ' ₸',
+                      style: TextStyle(fontSize: 18)),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text('Общая сумма:  ' + basket.allPrice.toString() + ' ₸',
+                  style: TextStyle(fontSize: 18)),
+            )
+          ],
+        ),
+        onTap: () {});
   }
 }
--- lib/views/currentPage/change_products_in_order.dart
+++ lib/views/currentPage/change_products_in_order.dart
@@ -1,5 +1,4 @@
 import 'package:boszhan_delivery_app/components/changeable_product_card.dart';
-import 'package:boszhan_delivery_app/components/product_card.dart';
 import 'package:boszhan_delivery_app/models/basket.dart';
 import 'package:boszhan_delivery_app/models/order.dart';
 import 'package:boszhan_delivery_app/services/orders_api_provider.dart';
@@ -15,11 +14,11 @@ class ChangeProductsInOrderPage extends StatefulWidget {
   final Order order;
 
   @override
-  _ChangeProductsInOrderPageState createState() => _ChangeProductsInOrderPageState();
+  _ChangeProductsInOrderPageState createState() =>
+      _ChangeProductsInOrderPageState();
 }
 
 class _ChangeProductsInOrderPageState extends State<ChangeProductsInOrderPage> {
-
   @override
   void initState() {
     super.initState();
@@ -45,118 +44,141 @@ class _ChangeProductsInOrderPageState extends State<ChangeProductsInOrderPage> {
           backgroundColor: Colors.white,
           appBar: PreferredSize(
               preferredSize: const Size.fromHeight(60.0),
-              child: buildAppBar('Выдача заказа')
-          ),
+              child: buildAppBar('Выдача заказа')),
           body: Column(
             children: [
               SizedBox(
-                height: MediaQuery.of(context).size.height*0.7,
+                height: MediaQuery.of(context).size.height * 0.7,
                 child: ListView.separated(
-                      itemCount: widget.order.basket.length,
-                      itemBuilder: (BuildContext context, int index) =>
-                      widget.order.basket[index].type == 0 ?
-                      InkWell(
-                        child: NotificationListener(
-                          child: ChangeableProductCard(widget.order.basket[index], index),
-                          onNotification: (message){
-                            int ind = AppConstants.index;
-                            String val = AppConstants.value;
-                            if (ind != -1){
-                              try {
-                                widget.order.basket[ind].count = double.parse(val);
-                              } on FormatException {
-                                null;
-                              }
-                            }
-                            return true;
-                          },
-                        ),
-                        onTap: () => displayAlertDialog(widget.order.basket[index].name, index)
-                      )
-                        : Ink(color: Colors.red[50],
-                          child: InkWell(
-                            child: NotificationListener(
-                              child:ChangeableProductCard(widget.order.basket[index], index),
-                              onNotification: (message){
-                                int ind = AppConstants.index;
-                                String val = AppConstants.value;
-                                if (ind != -1){
-                                  try {
-                                    widget.order.basket[ind].count = double.parse(val);
-                                  } on FormatException {
-                                    null;
+                  itemCount: widget.order.basket.length,
+                  itemBuilder: (BuildContext context, int index) =>
+                      widget.order.basket[index].type == 0
+                          ? InkWell(
+                              child: NotificationListener(
+                                child: ChangeableProductCard(
+                                    widget.order.basket[index], index),
+                                onNotification: (message) {
+                                  int ind = AppConstants.index;
+                                  String val = AppConstants.value;
+                                  if (ind != -1) {
+                                    try {
+                                      widget.order.basket[ind].count =
+                                          double.parse(val);
+                                    } on FormatException {
+                                      null;
+                                    }
                                   }
-                                }
-                                return true;
-                              },
-                            ),
-                            onTap: () => displayAlertDialog(widget.order.basket[index].name, index)
-                          )
-                        ),
-                      separatorBuilder: (context, index){
-                        return const Divider();
-                      },
-                  ),
+                                  return true;
+                                },
+                              ),
+                              onTap: () => displayAlertDialog(
+                                  widget.order.basket[index].name, index))
+                          : Ink(
+                              color: Colors.red[50],
+                              child: InkWell(
+                                  child: NotificationListener(
+                                    child: ChangeableProductCard(
+                                        widget.order.basket[index], index),
+                                    onNotification: (message) {
+                                      int ind = AppConstants.index;
+                                      String val = AppConstants.value;
+                                      if (ind != -1) {
+                                        try {
+                                          widget.order.basket[ind].count =
+                                              double.parse(val);
+                                          widget.order.basket[ind].allPrice =
+                                              widget.order.basket[ind].price *
+                                                  double.parse(val);
+                                        } on FormatException {
+                                          null;
+                                        }
+                                        print("Changed allPrice: " +
+                                            widget.order.basket[ind].allPrice
+                                                .toString());
+                                        print("Changed count: " +
+                                            widget.order.basket[ind].count
+                                                .toString());
+                                      }
+                                      return true;
+                                    },
+                                  ),
+                                  onTap: () => displayAlertDialog(
+                                      widget.order.basket[index].name, index))),
+                  separatorBuilder: (context, index) {
+                    return const Divider();
+                  },
+                ),
               ),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width*0.8,
-                        height: MediaQuery.of(context).size.width*0.1,
-                        child: ElevatedButton.icon(
-                          icon: const Icon(Icons.assignment_rounded, color: Colors.white),
-                          label: const Text('Отправить на сервер'),
-                          onPressed: () async {
-                            var connectivityResult = await (Connectivity().checkConnectivity());
-                            if (connectivityResult == ConnectivityResult.mobile) {
-                              setState(() {
-                                saveProducts(widget.order.basket);
-                              });
-                            } else if (connectivityResult == ConnectivityResult.wifi) {
-                              setState(() {
-                                saveProducts(widget.order.basket);
-                              });
-                            }
-                            else{
-                              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                                content: Text("Соединение с интернетом отсутствует.", style: TextStyle(fontSize: 20)),
-                              ));
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.green,
-                            textStyle: const TextStyle(color: Colors.white,fontSize: 20),
-                          ),
-                        ),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: MediaQuery.of(context).size.width * 0.1,
+                    child: ElevatedButton.icon(
+                      icon: const Icon(Icons.assignment_rounded,
+                          color: Colors.white),
+                      label: const Text('Отправить на сервер'),
+                      onPressed: () async {
+                        var connectivityResult =
+                            await (Connectivity().checkConnectivity());
+                        if (connectivityResult == ConnectivityResult.mobile) {
+                          setState(() {
+                            saveProducts(widget.order.basket);
+                          });
+                        } else if (connectivityResult ==
+                            ConnectivityResult.wifi) {
+                          setState(() {
+                            saveProducts(widget.order.basket);
+                          });
+                        } else {
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(const SnackBar(
+                            content: Text(
+                                "Соединение с интернетом отсутствует.",
+                                style: TextStyle(fontSize: 20)),
+                          ));
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        textStyle:
+                            const TextStyle(color: Colors.white, fontSize: 20),
                       ),
                     ),
-                  ]
-              ),
+                  ),
+                ),
+              ]),
               const Padding(
                 padding: EdgeInsets.all(10),
-                child: Text('Пока вы не отправите заказ на сервер, изменения не сохранятся.', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                child: Text(
+                    'Пока вы не отправите заказ на сервер, изменения не сохранятся.',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
               ),
             ],
           ),
-        )
-    );
+        ));
   }
 
-  void saveProducts(List<Basket> list) async{
+  void saveProducts(List<Basket> list) async {
     AppConstants.index = -1;
     String status = '';
-    OrdersProvider().changeBasket(widget.order.orderId.toString(), list).then((value) => status = value).whenComplete((){
-      if (status == 'Success'){
-        Navigator.pushAndRemoveUntil<dynamic>(context, MaterialPageRoute<dynamic>(
-          builder: (BuildContext context) => HomePage(),
-        ), (route) => false);
-      }
-      else{
+    OrdersProvider()
+        .changeBasket(widget.order.orderId.toString(), list)
+        .then((value) => status = value)
+        .whenComplete(() {
+      if (status == 'Success') {
+        Navigator.pushAndRemoveUntil<dynamic>(
+            context,
+            MaterialPageRoute<dynamic>(
+              builder: (BuildContext context) => HomePage(),
+            ),
+            (route) => false);
+      } else {
         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text("Something went wrong.", style: TextStyle(fontSize: 20)),
+          content:
+              Text("Something went wrong.", style: TextStyle(fontSize: 20)),
         ));
       }
     });
@@ -167,38 +189,32 @@ class _ChangeProductsInOrderPageState extends State<ChangeProductsInOrderPage> {
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: const Text('Внимание'),
-            content:
-            Text('Удалить продукт ' + name + ' из заказа?'),
-            actions: <Widget>[
-              FlatButton(
-                color: Colors.red,
-                textColor: Colors.white,
-                child: const Text('Нет'),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-              FlatButton(
-                color: Colors.green,
-                textColor: Colors.white,
-                child: const Text('Да'),
-                onPressed: () {
-                  deleteProductAction(index);
-                  Navigator.pop(context);
-                }
-              ),
-          ]);
-        }
-    );
+              title: const Text('Внимание'),
+              content: Text('Удалить продукт ' + name + ' из заказа?'),
+              actions: <Widget>[
+                FlatButton(
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  child: const Text('Нет'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                FlatButton(
+                    color: Colors.green,
+                    textColor: Colors.white,
+                    child: const Text('Да'),
+                    onPressed: () {
+                      deleteProductAction(index);
+                      Navigator.pop(context);
+                    }),
+              ]);
+        });
   }
 
-  void deleteProductAction(int index){
+  void deleteProductAction(int index) {
     setState(() {
       widget.order.basket.removeAt(index);
     });
   }
-
 }
-
-
